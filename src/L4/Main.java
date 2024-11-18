59901c16235e79f3d28ae9b6503f0d3d4de83fcd
Jack <jackn25@nycstudents.net>
2024-11-18 13:01:29 -0500

    did l4

---
+++ src/L4/Main.java
@@ -0,0 +1,17 @@
+package L4;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int j = 3; j < 6; j++)
+        {
+            int i = 1;
+            while (i <= j)
+            {
+                System.out.print("*");
+                i++;
+            }
+            System.out.println();
+        }
+
+    }
+}
