b5db300e150e62ec58145dda000633d86e00a399
School Github account for APCSP. <148903790+PeterG-08@users.noreply.github.com>
2024-11-24 20:57:13 +0000

    redoing init generation

--- src/MazeGenerator/Maze.java
+++ src/MazeGenerator/Maze.java
@@ -9,16 +9,6 @@ public class Maze {
     private final Node[][] nodeGrid;
     private final String[][] displayGrid;
 
-    private Coordinates currentCoords;
-
-    /**
-     * The coordinates of a single Node in the grid.
-     *
-     * @param x The x coordinate from the upper left corner (starts at 0).
-     * @param y The y coordinate from the upper left corner (starts at 0).
-     */
-    private record Coordinates(int x, int y) {}
-
     /**
      * Directions related to a single Node.
      */
@@ -30,35 +20,21 @@ public class Maze {
     }
 
     /**
-     * The node type.
+     * A node in the grid.
      */
-    private enum NodeType {
+    private enum Node {
         Cell("⬜"),
         Wall("⬛");
 
-        public final String display;
-
-        private NodeType(String display) {
-            this.display = display;
-        }
-    }
-
-    /**
-     * Represents a single node, can be a wall or a visited/unvisited cell.
-     */
-    private static class Node {
-        public final NodeType type;
-
         public boolean visited = false;
 
-        public Node(NodeType type) {
-            this.type = type;
-        }
+        public final String displayEmoji;
 
-        public String displayEmoji() {
-            return type.display;
+        private Node(String display) {
+            this.displayEmoji = display;
         }
     }
+
 
     /**
      * Creates a new Maze object with given parameters.
@@ -67,57 +43,44 @@ public class Maze {
      * @param height The height of the Maze.
      */
     public Maze(int width, int height) {
-        this.width = width;
-        this.height = height;
+        // ensure that width and height are odd
+        this.width = width % 2 == 1 ? width : width + 1;
+        this.height = height % 2 == 1 ? height : height + 1;
 
         displayGrid = new String[height][width];
         nodeGrid = new Node[height][width];
     }
 
     // get the node at the given coordinates
-    private Node getNode(Coordinates coords) {
-        return nodeGrid[coords.y][coords.x];
+    private Node getNode(int x, int y) {
+        return nodeGrid[y][x];
     }
 
-    // Returns the coordinates for the cell adjacent to a node. (ensures to jump over walls)
-    private Coordinates adjacentNode(Coordinates coords, Direction direction) {
-        return switch (direction) {
-            case UP -> new Coordinates(coords.x, coords.y - 2);
-
-            case DOWN -> new Coordinates(coords.x, coords.y + 2);
-
-            case LEFT -> new Coordinates(coords.x - 2, coords.y);
-
-            case RIGHT -> new Coordinates(coords.x + 2, coords.y);
-
-            default -> new Coordinates(0, 0);
-        };
-    }
 
     // reset the node grid to a grid of cells and walls
     private void resetNodeGrid() {
-        // upper and lower rows of just walls
-        Node[] closingRow = new Node[width];
-        Arrays.fill(closingRow, new Node(NodeType.Wall));
+        Node[] wallRow = new Node[width];
+        Arrays.fill(wallRow, Node.Wall);
 
-        Node[] regularRow = new Node[width];
+        Node[] cellRow = new Node[width];
 
-        // create a regular row of alternating wall and cell nodes
-        for (int i=0; i < width; i++) {
-            if (i % 2 == 0) {
-                regularRow[i] = new Node(NodeType.Wall);
+        // fill in cell row
+        for (int j = 0; j < width; j++) {
+            if (j % 2 == 1) {
+                cellRow[j] = Node.Cell;
             } else {
-                regularRow[i] = new Node(NodeType.Cell);
+                cellRow[j] = Node.Wall;
             }
         }
 
-        // set upper and lower rows
-        nodeGrid[0] = closingRow;
-        nodeGrid[height - 1] = closingRow;
+        // fill everything with walls initially
+        for (int i = 0; i < height; i++) {
+            nodeGrid[i] = wallRow;
+        }
 
-        // set every other row
-        for (int i=1; i < height-1; i++) {
-            nodeGrid[i] = regularRow;
+        // then alternate with cell row
+        for (int i = 1; i < height - 1; i+=2) {
+            nodeGrid[i] = cellRow;
         }
     }
 
@@ -125,7 +88,7 @@ public class Maze {
     private void resetDisplayGrid() {
         for (int i=0; i < height; i++) {
             for (int j=0; j < width; j++) {
-                displayGrid[i][j] = nodeGrid[i][j].displayEmoji();
+                displayGrid[i][j] = nodeGrid[i][j].displayEmoji;
             }
         }
     }
@@ -146,17 +109,7 @@ public class Maze {
     /**
      * Regenerates the maze.
      */
-    public void regenerate() {
-        for (Direction direction : Direction.values()) {
-            Coordinates adjacentCoords = adjacentNode(currentCoords, direction);
-            Node adjacent = getNode(adjacentCoords); // TODO null warning thing
-
-            if (!adjacent.visited) {
-                currentCoords = adjacentCoords;
-                regenerate();
-            }
-        }
-    }
+    public void regenerate(int startX, int startY) {}
 
     /**
      * Clears and resets the grid to make a grid of walls and cells.
