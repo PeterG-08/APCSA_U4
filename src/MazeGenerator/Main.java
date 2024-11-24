34c4a655b50de909af746a5b84f2e48d33ce2b9e
School Github account for APCSP. <148903790+PeterG-08@users.noreply.github.com>
2024-11-24 23:17:15 +0000

    works with int arrays, about to try with old enum way

--- src/MazeGenerator/Main.java
+++ src/MazeGenerator/Main.java
@@ -2,9 +2,14 @@ package MazeGenerator;
 
 public class Main {
     public static void main(String[] args) {
-        Maze maze = new Maze(11, 11);
+        Maze maze = new Maze(5, 5);
 
-        maze.reset();
+        // maze.display();
+        // System.out.println();
+
+        maze.regenerate(0, 0);
+        // maze.setNode(0, 0, )
+
         maze.display();
     }
 }
--- src/MazeGenerator/Maze.java
+++ src/MazeGenerator/Maze.java
@@ -1,12 +1,27 @@
 package MazeGenerator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Maze {
+    private final static int CELL = 1;
+    private final static int VISITED = 2;
+    private final static int WALL = 3;
+
+    private final static Map<Integer, String> EMOJIS = new HashMap<Integer, String>() {{
+        put(CELL, "⬜");
+        put(VISITED, "⬜");
+        put(WALL, "⬛");
+    }};
+
     private final int width;
     private final int height;
 
-    private final Node[][] nodeGrid;
+    private final int[][] nodeGrid;
     private final String[][] displayGrid;
 
     /**
@@ -20,67 +35,45 @@ public class Maze {
     }
 
     /**
-     * A node in the grid.
-     */
-    private enum Node {
-        Cell("⬜"),
-        Wall("⬛");
-
-        public boolean visited = false;
-
-        public final String displayEmoji;
-
-        private Node(String display) {
-            this.displayEmoji = display;
-        }
-    }
-
-
-    /**
      * Creates a new Maze object with given parameters.
      *
      * @param width The width of the Maze.
      * @param height The height of the Maze.
      */
     public Maze(int width, int height) {
-        // ensure that width and height are odd
-        this.width = width % 2 == 1 ? width : width + 1;
-        this.height = height % 2 == 1 ? height : height + 1;
+        this.width = width;
+        this.height = height;
 
         displayGrid = new String[height][width];
-        nodeGrid = new Node[height][width];
+        nodeGrid = new int[height][width];
+
+        reset();
     }
 
-    // get the node at the given coordinates
-    private Node getNode(int x, int y) {
+
+    private int getNode(int x, int y) {
         return nodeGrid[y][x];
     }
 
+    public void setNode(int x, int y, int node) {
+        nodeGrid[y][x] = node;
+    }
+
+    private boolean inBounds(int x, int y) {
+        return 0 <= x && x <= width - 1 && 0 <= y && y <= height - 1;
+    }
 
     // reset the node grid to a grid of cells and walls
     private void resetNodeGrid() {
-        Node[] wallRow = new Node[width];
-        Arrays.fill(wallRow, Node.Wall);
-
-        Node[] cellRow = new Node[width];
-
-        // fill in cell row
-        for (int j = 0; j < width; j++) {
-            if (j % 2 == 1) {
-                cellRow[j] = Node.Cell;
-            } else {
-                cellRow[j] = Node.Wall;
-            }
-        }
-
-        // fill everything with walls initially
         for (int i = 0; i < height; i++) {
-            nodeGrid[i] = wallRow;
-        }
-
-        // then alternate with cell row
-        for (int i = 1; i < height - 1; i+=2) {
-            nodeGrid[i] = cellRow;
+            for (int j = 0; j < width; j++) {
+                // if (j % 2 == 1 && i % 2 == 1) {
+                //     setNode(j, i, CELL);
+                // } else {
+                //     setNode(j, i, WALL);
+                // }
+                setNode(j, i, WALL);
+            }
         }
     }
 
@@ -88,7 +81,7 @@ public class Maze {
     private void resetDisplayGrid() {
         for (int i=0; i < height; i++) {
             for (int j=0; j < width; j++) {
-                displayGrid[i][j] = nodeGrid[i][j].displayEmoji;
+                displayGrid[i][j] = EMOJIS.get(nodeGrid[i][j]);
             }
         }
     }
@@ -109,7 +102,82 @@ public class Maze {
     /**
      * Regenerates the maze.
      */
-    public void regenerate(int startX, int startY) {}
+    public void regenerate(int x, int y) {
+        // this node is now a visited cell
+        setNode(x, y, VISITED);
+
+        List<Direction> possibleDirections = new ArrayList<Direction>(Arrays.asList(Direction.values()));
+
+        // check all directions
+        while (possibleDirections.size() > 0) {
+            // choose random direction
+            int index = (int) (Math.random() * possibleDirections.size());
+            Direction direction = possibleDirections.get(index);
+
+            // remove the direction as a possible choice
+            possibleDirections.remove(index);
+
+            // new coordinates
+            int adjX = 0;
+            int adjY = 0;
+    
+            int newX = 0;
+            int newY = 0;
+
+            switch (direction) {
+                case UP:
+                    adjY = y - 1;
+                    adjX = x;
+
+                    newY = y - 2;
+                    newX = x;
+                    break;
+            
+                case DOWN:
+                    adjY = y + 1;
+                    adjX = x;
+
+                    newY = y + 2;
+                    newX = x;
+                    break;
+                
+                case LEFT:
+                    adjY = y;
+                    adjX = x - 1;
+
+                    newY = y;
+                    newX = x - 2;
+                    break;
+
+                case RIGHT:
+                    adjY = y;
+                    adjX = x + 1;
+
+                    newY = y;
+                    newX = x + 2;
+                    break;
+
+                default:
+                    break;
+            }
+
+            // if out of bounds, check another direction
+            if (!inBounds(newX, newY)) {
+                continue;
+            }
+
+            // if possible to carve in this direction, then do it
+            if (!(getNode(newX, newY) == VISITED)) {
+                setNode(adjX, adjY, VISITED);
+                
+                regenerate(newX, newY);
+            }
+        }
+
+        // if none of the directions worked, then the maze has been generated!
+        resetDisplayGrid();
+        return;
+    }
 
     /**
      * Clears and resets the grid to make a grid of walls and cells.
